a1bae398aaddb770b8f87536823b4c95e389ed74
AryusG <verdyg24@gmail.com>
2021-06-14 23:53:08 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Variables
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.minsize(width=850, height=576)
+window.config(padx=50, pady=50)
+
+imag
+
+window.mainloop()
